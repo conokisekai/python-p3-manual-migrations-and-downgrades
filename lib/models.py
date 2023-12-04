313000f454759e8e2eec59d9842884bc829e76ed
Conrad Kambi <kambiconrad@gmail.com>
2023-12-05 00:04:41 +0300

    assignmnet Completed

--- lib/models.py
+++ lib/models.py
@@ -3,13 +3,13 @@ import sys
 
 sys.path.append(os.getcwd())
 
-
 from datetime import datetime
 
 from sqlalchemy import create_engine, desc
-from sqlalchemy import (CheckConstraint, UniqueConstraint,
-    Column, DateTime, Integer, String)
-
+from sqlalchemy import (
+    CheckConstraint, UniqueConstraint,
+    Column, DateTime, Integer, String
+)
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///migrations_test.db')
@@ -22,11 +22,11 @@ class Student(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String(), index=True)
     email = Column(String(55))
-    grade = Column(Integer())
+    class_level = Column(Integer())  # Renamed 'grade' to 'class_level'
     birthday = Column(DateTime())
     enrolled_date = Column(DateTime(), default=datetime.now())
 
     def __repr__(self):
         return f"Student {self.id}: " \
-            + f"{self.name}, " \
-            + f"Grade {self.grade}"
+               + f"{self.name}, " \
+               + f"Class Level {self.class_level}"  # Updated repr to use 'class_level'
